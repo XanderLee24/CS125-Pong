a33dc07d18cf61c5ca9a4dbba8f6b88cf7582abe
Xander Lee <xanderlee2424@gmail.com>
2026-03-31 11:52:06 -0700

    Add main game loop and screen management for Pong

---
+++ Pong.c
@@ -0,0 +1,73 @@
+#include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "Functions.h"
+
+int main()
+{
+    // Initialization
+    int currentscreen = 0;
+    double startTime;
+
+    InitWindow(screenWidth, screenHeight, "Pong window");
+    srand(time(NULL));
+    InitGame();
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        BeginDrawing();
+
+        BuildMapBasic(screenWidth, screenHeight); // Custom Function
+
+        switch (currentscreen) {
+            case 0: {
+                // Start Screen
+                DrawText("Press Enter to Start Game!", 270, 400, 30, WHITE);
+                if (IsKeyPressed(KEY_ENTER))
+                    currentscreen = 1;
+                    startTime = 0;  //reset timer in case the player goes back to the title screen
+                break;
+            }
+
+            case 1: {
+                //timer
+                if (startTime == 0)
+                    startTime = GetTime();   // start timer once
+
+                double elapsed = GetTime() - startTime;
+                int countdown = 3 - (int)elapsed;
+
+                if (countdown > 0)
+                {
+                    char text[10];
+                    sprintf(text, "%d", countdown);
+                    DrawText(text, 495, 350, 80, WHITE);
+                }
+                else
+                {
+                    currentscreen = 2;
+                }
+                break;
+            }
+            case 2: {
+                //playGame()
+                GamePlay();
+                break;
+            }
+        }
+
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    return 0;
+}
